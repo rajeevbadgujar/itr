0d2f789cf610f67f1482c1cab15b89fa0a93e880
ankitgr8 <ankitgr8@gmail.com>
2014-09-30 19:27:04 +0530

    delete the old code and jre

--- src/in/gov/incometaxindiaefiling/ws/ds/ex/v_1_0/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://incometaxindiaefiling.gov.in/ws/ds/ex/v_1_0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package in.gov.incometaxindiaefiling.ws.ds.ex.v_1_0;
--- src/in/gov/incometaxindiaefiling/ws/ds/itrv/v_1_0/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://incometaxindiaefiling.gov.in/ws/ds/itrv/v_1_0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package in.gov.incometaxindiaefiling.ws.ds.itrv.v_1_0;
